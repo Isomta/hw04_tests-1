f84d97ea20d91985c288b46b3c9c76734622bebc
Алексей Щербаков <shav@192.168.0.24>
2022-06-01 01:00:52 +0500

    13

--- yatube/core/context_processors/var.py
+++ yatube/core/context_processors/var.py
@@ -3,3 +3,6 @@ import datetime
 
 def year(request):
     return {'year': str((datetime.datetime.now()))[:4]}
+
+def time(request):
+    return {'time': str((datetime.datetime.now().strftime("%H:%M:%S")))}
